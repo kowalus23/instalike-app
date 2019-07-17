ff28dfe3979d414d36c6332f8ced263d2f715c35
kowalus23 <kowalus23@gmail.com>
2019-07-17 13:34:43 +0200

    non of changes, trying to add modal to confirm delete button on cards

--- src/components/PhotoCard.js
+++ src/components/PhotoCard.js
@@ -18,35 +18,36 @@ const PhotoCard = ({post, removePost, index, comments, startRemovingPost}) => {
     }
   };
 
-
   return (
-    <div className="card custom mx-3 mb-4" style={hover ? cardStyle.hover : null}>
-      <div className="card-img-top border-bottom custom">
-        <Link to={`photo/${post.id}`}>
-          <img
-            className="img-fluid-custom"
-            src={post.imageLink.indexOf('http') ? imagePlaceholder : post.imageLink}
-            alt="ownPicture"/>
-        </Link>
-      </div>
-      <div className="card-body">
-        <div className="d-flex justify-content-between position-relative">
-          <p className="card-text --list mb-0 pr-3">{post.description}</p>
-          <Link className="custom--card-content" to={`photo/${post.id}`}>
-              <i className="far fa-comment"/> <span>{comments[post.id] ? comments[post.id].length + 2  : 2}</span>
+    <React.Fragment>
+      <div className="card custom mx-3 mb-4" style={hover ? cardStyle.hover : null}>
+        <div className="card-img-top border-bottom custom">
+          <Link to={`photo/${post.id}`}>
+            <img
+              className="img-fluid-custom"
+              src={post.imageLink.indexOf('http') ? imagePlaceholder : post.imageLink}
+              alt="ownPicture"/>
           </Link>
         </div>
-        <button
-          onMouseEnter={toggleHover}
-          onMouseLeave={toggleHover}
-          onClick={() => {
-            startRemovingPost(index, post.id)
-          }}
-          className="btn btn-outline-danger">
-          X
-        </button>
+        <div className="card-body">
+          <div className="d-flex justify-content-between position-relative">
+            <p className="card-text --list mb-0 pr-3">{post.description}</p>
+            <Link className="custom--card-content" to={`photo/${post.id}`}>
+              <i className="far fa-comment"/> <span>{comments[post.id] ? comments[post.id].length + 2 : 2}</span>
+            </Link>
+          </div>
+          <button
+            onMouseEnter={toggleHover}
+            onMouseLeave={toggleHover}
+            onClick={() => {
+              startRemovingPost(index, post.id)
+            }}
+            className="btn btn-outline-danger">
+            X
+          </button>
+        </div>
       </div>
-    </div>
+    </React.Fragment>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -19,4 +19,3 @@ ReactDOM.render(
     </Router>
   </Provider>
   , document.getElementById('root'));
-
