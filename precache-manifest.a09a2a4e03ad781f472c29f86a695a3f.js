82fc000684b4b01f5db88c50d1c00f1d279e6795
kowalus23 <kowalus23@gmail.com>
2019-07-17 19:31:49 +0200

    Updates

---
+++ precache-manifest.a09a2a4e03ad781f472c29f86a695a3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f419848b7bab5a15f2ea8287f915bba4",
+    "url": "/instalike-app/index.html"
+  },
+  {
+    "revision": "dd2cdcf6ae453fae2ea1",
+    "url": "/instalike-app/static/css/main.259b02f5.chunk.css"
+  },
+  {
+    "revision": "25664d248fd511501adf",
+    "url": "/instalike-app/static/js/2.43e240b8.chunk.js"
+  },
+  {
+    "revision": "dd2cdcf6ae453fae2ea1",
+    "url": "/instalike-app/static/js/main.14f31d37.chunk.js"
+  },
+  {
+    "revision": "c6fc5de04270f8324582",
+    "url": "/instalike-app/static/js/runtime~main.f0dcc1e2.js"
+  }
+]);
